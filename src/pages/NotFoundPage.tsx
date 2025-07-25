002cbaf61e980ccd5f50fb92de16f03dced1f42c
rjbelleza <rjbelleza19@gmail.com>
2025-07-25 17:45:38 +0800

    Setup: Routes with react router

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+
+const NotFoundPage = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default NotFoundPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,4 +2,24 @@ import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 
+const HomePage = lazy(() => import('../pages/HomePage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
+const MainLayout = lazy(() => import('../layouts/MainLayout'));
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        errorElement: <Suspense fallback={<div>Loading...</div>}><NotFoundPage /></Suspense>,
+        children: [
+            {
+                index: true,
+                element: <Suspense fallback={<div>Loading...</div>}><HomePage /></Suspense>
+            }
+        ]
+    }
+]);
+
+export default router;
